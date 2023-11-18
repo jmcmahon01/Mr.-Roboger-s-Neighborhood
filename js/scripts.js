9e6fe7b5bdfe913d162f0cd68248ead14aabf4a0
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-18 10:38:04 -0800

    add UI logic to display results on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
+//Business Logic
 function isNumber(inputNumber) {
-  const userInput = parseInt(inputNumber);
-  if (typeof inputNumber !== 'number' || isNaN(inputNumber)) {
+  if (typeof userInput !== 'number' || isNaN(userInput)) {
     return "Please enter a number";
   }
 }
 
-function countToNumber() {
+function countToNumber(inputNumber) {
   let returnArray = [];
   for (let i = 0; i <= inputNumber; i++) {
     let element = '';
@@ -22,3 +22,45 @@ function countToNumber() {
   }
   return returnArray;
 }
+//User Interface logic
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("numberForm");
+  const mrRodgers = document.getElementById("mrRodgers");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputNumber = document.getElementById("inputNumber").value;
+
+    // Convert inputNumber to a number
+    const number = parseInt(inputNumber);
+
+    // Clear existing content in mrRodgers div
+    mrRodgers.innerHTML = '';
+
+    if (isNaN(number)) {
+      // Display the error message directly in the mrRodgers div
+      const errorMessageElement = document.createElement('p');
+      errorMessageElement.textContent = "Please enter a number";
+      mrRodgers.appendChild(errorMessageElement);
+    } else {
+      // Generate the array
+      const resultArray = countToNumber(number);
+
+      // Display the result in the mrRodgers div
+      const resultParagraph = document.createElement('p');
+      resultParagraph.textContent = "I... AM.... Mr. RODGERS... BEEP BOOP BEEP. I... CAN... COUNT.... TO YOUR... NUMBER: ";
+      mrRodgers.appendChild(resultParagraph);
+
+      // Display the array elements
+      for (const element of resultArray) {
+        const elementParagraph = document.createElement('p');
+        elementParagraph.textContent = element;
+        mrRodgers.appendChild(elementParagraph);
+      }
+
+      // Remove the "hidden" class to show the mrRodgers div
+      mrRodgers.classList.remove("hidden");
+    }
+  });
+});
