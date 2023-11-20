f4efe162fb63df5ac81e371fea39184ddbe26038
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-20 08:17:01 -0800

    refactor/DRY UI logic so it is easier to read

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business Logic
 function isNumber(inputNumber) {
-  if (typeof userInput !== 'number' || isNaN(userInput)) {
+  if (isNaN(inputNumber)) {
     return "Please enter a number";
   }
 }
@@ -22,45 +22,27 @@ function countToNumber(inputNumber) {
   }
   return returnArray;
 }
-//User Interface logic
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("numberForm");
+
+window.addEventListener("load", function () {
+  const form = document.querySelector("form");
   const mrRodgers = document.getElementById("mrRodgers");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
+  const resultP = document.getElementById("resultP");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
     const inputNumber = document.getElementById("inputNumber").value;
-
-    // Convert inputNumber to a number
-    const number = parseInt(inputNumber);
-
-    // Clear existing content in mrRodgers div
-    mrRodgers.innerHTML = '';
-
-    if (isNaN(number)) {
-      // Display the error message directly in the mrRodgers div
-      const errorMessageElement = document.createElement('p');
-      errorMessageElement.textContent = "Please enter a number";
-      mrRodgers.appendChild(errorMessageElement);
-    } else {
-      // Generate the array
-      const resultArray = countToNumber(number);
-
-      // Display the result in the mrRodgers div
-      const resultParagraph = document.createElement('p');
-      resultParagraph.textContent = "I... AM.... Mr. RODGERS... BEEP BOOP BEEP. I... CAN... COUNT.... TO YOUR... NUMBER: ";
-      mrRodgers.appendChild(resultParagraph);
-
-      // Display the array elements
-      for (const element of resultArray) {
-        const elementParagraph = document.createElement('p');
-        elementParagraph.textContent = element;
-        mrRodgers.appendChild(elementParagraph);
-      }
-
-      // Remove the "hidden" class to show the mrRodgers div
-      mrRodgers.classList.remove("hidden");
+    const errorMessage = isNumber(inputNumber);
+    const returnArray = countToNumber(parseInt(inputNumber));
+    if (errorMessage) {
+      document.getElementById("error").removeAttribute("class");
+    } else if (returnArray) {
+      resultP.textContent += ' ' + returnArray.join(', ');
+      mrRodgers.removeAttribute("class");
     }
   });
 });
+
+
+
+
+
+
